e4f8f69688cb70bf5948ff999e7c04491639d74d
antbrown999 <164958480+antbrown999@users.noreply.github.com>
2025-02-03 23:35:48 -0600

    Add files via upload

---
+++ Logger.js
@@ -0,0 +1,12 @@
+// Exercise 7 pt 2
+class logger {
+    constructor(Referrer, id) {
+        this.referrer = this.referrer
+        this.id = id
+    }
+
+    log = (value) => {
+        console.log(`[${this.referrer}:${this.id}`)
+    }
+
+}
